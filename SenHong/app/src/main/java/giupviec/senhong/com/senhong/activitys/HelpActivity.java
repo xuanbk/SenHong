4d2b1966492158288c9e9f0e1b955a9eaf21e38e
Xuan Le Van <xuanlv@newwindsoft.com>
2017-04-25 16:55:28 +0700

    payment

--- SenHong/app/src/main/java/giupviec/senhong/com/senhong/activitys/HelpActivity.java
+++ SenHong/app/src/main/java/giupviec/senhong/com/senhong/activitys/HelpActivity.java
@@ -1,10 +1,18 @@
 package giupviec.senhong.com.senhong.activitys;
 
+import android.app.AlertDialog;
+import android.content.DialogInterface;
+import android.content.Intent;
+import android.content.pm.PackageInfo;
+import android.content.pm.PackageManager;
+import android.net.Uri;
 import android.os.Bundle;
 import android.support.annotation.Nullable;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
 import android.view.View;
+import android.widget.TextView;
+import android.widget.Toast;
 
 import giupviec.senhong.com.senhong.R;
 
@@ -13,7 +21,9 @@ import giupviec.senhong.com.senhong.R;
  * Created by Administrator on 04/24/2017.
  */
 
-public class HelpActivity extends AppCompatActivity {
+public class HelpActivity extends AppCompatActivity implements View.OnClickListener {
+    private TextView tvLikeFace, tvVisit, tvTerm, tvVersion, tvPay;
+
     @Override
     protected void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -28,7 +38,20 @@ public class HelpActivity extends AppCompatActivity {
                 onBackPressed();
             }
         });
+        initUI();
+    }
 
+    private void initUI() {
+        tvLikeFace = (TextView) findViewById(R.id.tvLikeFace);
+        tvVisit = (TextView) findViewById(R.id.tvVisit);
+        tvTerm = (TextView) findViewById(R.id.tvTerm);
+        tvVersion = (TextView) findViewById(R.id.tvVersion);
+        tvPay = (TextView) findViewById(R.id.tvPay);
+        tvLikeFace.setOnClickListener(this);
+        tvVisit.setOnClickListener(this);
+        tvTerm.setOnClickListener(this);
+        tvVersion.setOnClickListener(this);
+        tvPay.setOnClickListener(this);
     }
 
     @Override
@@ -36,4 +59,45 @@ public class HelpActivity extends AppCompatActivity {
         super.onBackPressed();
     }
 
+    @Override
+    public void onClick(View view) {
+        Intent browserIntent;
+        switch (view.getId()) {
+            case R.id.tvLikeFace:
+                browserIntent = new Intent(Intent.ACTION_VIEW, Uri.parse("http://www.facebook.com"));
+                startActivity(browserIntent);
+                break;
+            case R.id.tvVisit:
+                browserIntent = new Intent(Intent.ACTION_VIEW, Uri.parse("http://www.google.com"));
+                startActivity(browserIntent);
+                break;
+            case R.id.tvTerm:
+                browserIntent = new Intent(Intent.ACTION_VIEW, Uri.parse("http://www.google.com"));
+                startActivity(browserIntent);
+                break;
+            case R.id.tvVersion:
+                PackageInfo pInfo = null;
+                try {
+                    pInfo = getPackageManager().getPackageInfo(getPackageName(), 0);
+                } catch (PackageManager.NameNotFoundException e) {
+                    e.printStackTrace();
+                }
+                String version = pInfo.versionName;
+                new AlertDialog.Builder(HelpActivity.this)
+                        .setMessage("Phiên bản : " + version)
+                        .setCancelable(false)
+                        .setPositiveButton("ok", new DialogInterface.OnClickListener() {
+                            @Override
+                            public void onClick(DialogInterface dialog, int which) {
+                                dialog.dismiss();
+                            }
+                        }).show();
+                break;
+            case R.id.tvPay:
+                startActivity(new Intent(HelpActivity.this, PaymentActivity.class));
+                break;
+            default:
+                break;
+        }
+    }
 }
---
+++ SenHong/app/src/main/java/giupviec/senhong/com/senhong/activitys/PaymentActivity.java
@@ -0,0 +1,39 @@
+package giupviec.senhong.com.senhong.activitys;
+
+import android.os.Bundle;
+import android.support.annotation.Nullable;
+import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.Toolbar;
+import android.view.View;
+
+import giupviec.senhong.com.senhong.R;
+
+
+/**
+ * Created by Administrator on 04/24/2017.
+ */
+
+public class PaymentActivity extends AppCompatActivity {
+    @Override
+    protected void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_payment);
+        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
+        toolbar.setTitle("THANH TOÁN");
+        toolbar.setNavigationIcon(R.drawable.ic_back);
+        setSupportActionBar(toolbar);
+        toolbar.setNavigationOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                onBackPressed();
+            }
+        });
+
+    }
+
+    @Override
+    public void onBackPressed() {
+        super.onBackPressed();
+    }
+
+}
